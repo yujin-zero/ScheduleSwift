317dffb6e58c33a007b4095dee4578c1458380c8
hayeon <rlagkdus@hayeonui-MacBookPro.local>
2023-05-05 13:52:44 +0900

    강의 등록 화면

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Test from "./pages/Test";
 import Test2 from "./pages/Test2";
 import Start from "./pages/Start";
 import LoginPage from "./pages/LoginPage"
+import Lectureapply from "./pages/Lectureapply"
+
+
 function App() {
   return (
     <BrowserRouter>
         <div className="App">
           <Routes>
-            <Route path="/" element={<Start />}/>
+            <Route path="/start" element={<Start />}/>
             <Route path="/test" element={<Test />}/>
             <Route path="/test2" element={<Test2 />}/>
             <Route path="/loginpage" element={<LoginPage />}/>
+            <Route path="/" element={<Lectureapply />}/>
           </Routes>
         </div>
     </BrowserRouter>
