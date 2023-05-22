306956daea0eb37b4d3dbe192b231c72772d6b73
soyujin <poly30@naver.com>
2023-05-22 18:29:57 +0900

    시간표, 증원요청페이지

--- src/pages/JoinPage.jsx
+++ src/pages/JoinPage.jsx
@@ -78,40 +78,8 @@ const JoinPage = () => {
                 <select id="dropdown" className="join_input_content" value={department} onChange={handleDropdownChange}>
                   <option value="">-- 학과를 선택해주세요. --</option>
                   <option value="소프트웨어학과">소프트웨어학과</option>
-                  <option value="컴퓨터공학과">컴퓨터공학과</option>
-                  <option value="정보보호학과">정보보호학과</option>
-                  <option value="데이터사이언스학과">데이터사이언스학과</option>
-                  <option value="지능기전공학부">지능기전공학부</option>
-                  <option value="디자인이노베이션전공">디자인이노베이션전공</option>
-                  <option value="만화애니메이션텍전공">만화애니메이션텍전공</option>
-                  <option value="회화과">회화과</option>
-                  <option value="패션디자인학과">패션디자인학과</option>
-                  <option value="음악과">음악과</option>
-                  <option value="체육학과">체육학과</option>
-                  <option value="무용과">무용과</option>
-                  <option value="영화예술학과">영화예술학과</option>
-                  <option value="건축공학부">건축공학부</option>
-                  <option value="건설환경공학과">건설환경공학과</option>
-                  <option value="환경에너지공간융합학과">환경에너지공간융합학과</option>
-                  <option value="지구자원시스템공학과">지구자원시스템공학과</option>
-                  <option value="나노신소재공학과">나노신소재공학과</option>
-                  <option value="양자원자력공학과">양자원자력공학과</option>
-                  <option value="국방시스템공학과">국방시스템공학과</option>
-                  <option value="기계항공우주공학부">기계항공우주공학부</option>
-                  <option value="항공시스템공학과">항공시스템공학과</option>
-                  <option value="법학부">법학부</option>
-                  <option value="수학통계학부">수학통계학부</option>
-                  <option value="물리천문학과">물리천문학과</option>
-                  <option value="화학과">화학과</option>
-                  <option value="생명시스템학부">생명시스템학부</option>
-                  <option value="국어국문학과">국어국문학과</option>
-                  <option value="국제학부">국제학부</option>
-                  <option value="역사학과">역사학과</option>
-                  <option value="교육학과">교육학과</option>
-                  <option value="행정학과">행정학과</option>
-                  <option value="미디어커뮤니케이션학과">미디어커뮤니케이션학과</option>
-                  <option value="경영학부">경영학부</option>
-                  <option value="경제학과">경제학과</option>
+                  
+                  
                 </select>
             </div>
             <div className="join_input_container">
---
+++ src/pages/LectureData.js
@@ -0,0 +1,26 @@
+export const getAllData = () => {
+    let LectureData = [
+        {
+            subject: "과목1",
+            t_lecture: "수 13:30~15:00",
+        },
+        {
+            subject: "과목2",
+            t_lecture: "수 금 11:00~12:00",
+        },
+        {
+            subject: "과목3",
+            t_lecture: "화 14:00~15:00",
+        },
+        {
+            subject: "과목4",
+            t_lecture: "월 14:00~15:00",
+        },
+        {
+            subject: "과목5",
+            t_lecture: "화 목 09:00~10:30",
+        },
+    ];
+
+    return LectureData;
+};
